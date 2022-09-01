95c2fe8773eabec6fb1a228052a47c6aa7096b1a
Jinson <web2feel@gmail.com>
2022-09-01 19:46:40 +0530

    Lesson 4 final update

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,35 +1,58 @@
 import React from "react";
-import {currencyFormatter} from "../util"
+import { currencyFormatter } from "../util";
 
-function ProductItem({ id,title,price,image,cart,setCart }) { 
+function ProductItem({ id, title, price, image, cart, setCart }) {
+  
+  const isInCart = (_id) => {
+    return !!cart.find((item) => item.id === _id);
+  };
 
+  let productCount = cart.find(item => item.id === id)?.count
+
+  const countHandler = (_id) => {
+    let itemIndex = cart.findIndex(item => item.id === _id)
+     setCart((prev) => {
+      return prev.map((item,index) =>{
+          if(index === itemIndex){
+            return {...item, count: item.count++}
+          }
+          return item
+      })
+    })
+  }
+
+  
   const cartHandler = () => {
-
     setCart((prevState) => {
       let cartProduct = {
         id,
         title,
         price,
-        image
-      }
-      return [...prevState, cartProduct]
-    })
-
- 
-
-  }
+        image,
+        count: 1,
+      };
+      return [...prevState, cartProduct];
+    });
+  };
 
   return (
     <div className="productItem">
       <div className="itemName">
-        <h3>{title}</h3>        
-        </div>
+        <h3>{title}</h3>
+      </div>
       <div className="itemPic">
-        <img src={`/images/${image}`} alt=""/>
+        <img src={`/images/${image}`} alt="" />
       </div>
       <div className="itemMeta">
         <div className="itemPrice"> {currencyFormatter(price)} </div>
-        <button className="cartButton" onClick={cartHandler}>Add to Cart</button>
+
+        {isInCart(id) ? (
+          <button className="cartButton" onClick={() => countHandler(id)} > {productCount} Added to Cart </button>
+        ) : (
+          <button className="cartButton" onClick={cartHandler}>
+            Add to Cart
+          </button>
+        )}
       </div>
     </div>
   );
